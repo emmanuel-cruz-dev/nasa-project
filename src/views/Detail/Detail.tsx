24c4d827cbe5b5c985b3204d9fa1339dae6cded0
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-01-27 13:40:31 -0300

    estilos en Detail component

--- src/views/Detail/Detail.tsx
+++ src/views/Detail/Detail.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
 import { useRoute } from "@react-navigation/native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RouteStackParams } from "../../types";
@@ -17,7 +17,9 @@ const Detail = () => {
         <Image source={{ uri: url }} style={styles.image} />
         <Text style={styles.title}>{title}</Text>
         <Text style={styles.date}>{date}</Text>
-        <Text style={styles.explanation}>{explanation}</Text>
+        <ScrollView style={styles.explanationContainer}>
+          <Text style={styles.explanation}>{explanation}</Text>
+        </ScrollView>
       </View>
     </View>
   );
@@ -29,9 +31,36 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
     backgroundColor: "rgba(7,26,93,255)",
   },
+  content: {
+    flex: 1,
+    backgroundColor: "#2c449d",
+    borderRadius: 32,
+    marginVertical: 24,
+    padding: 16,
+  },
   title: {
     fontSize: 24,
     fontWeight: "bold",
+    color: "#fff",
+    marginBottom: 12,
+  },
+  image: {
+    width: "100%",
+    height: "40%",
+    borderColor: "#fff",
+    borderWidth: 2,
+    borderRadius: 32,
+    marginBottom: 16,
+  },
+  date: {
+    fontSize: 16,
+    color: "#fff",
+  },
+  explanationContainer: {
+    marginVertical: 16,
+  },
+  explanation: {
+    fontSize: 16,
     color: "#fff",
   },
 });

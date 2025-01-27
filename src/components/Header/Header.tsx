05aeb510517eb24a4c7cf392af1e19ca0351f77e
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-01-27 08:41:29 -0300

    estilos en Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,45 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 
 const Header = () => {
   return (
-    <View>
-      <View>
-        <Image source={require("../../assets/nasa-logo.png")} />
+    <View style={styles.container}>
+      <View style={styles.leftContainer}>
+        <Text style={styles.title}>Explore</Text>
       </View>
-      <View>
-        <Text>Explore</Text>
+      <View style={styles.rightContainer}>
+        <Image
+          source={require("../../assets/nasa-logo.png")}
+          style={styles.image}
+        />
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    // flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  leftContainer: {
+    // flex: 1,
+    alignItems: "flex-start",
+  },
+  rightContainer: {
+    // flex: 1,
+    alignItems: "flex-end",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+  image: {
+    width: 60,
+    height: 60,
+  },
+});
 
 export default Header;
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,12 +1,19 @@
-import { View, Text } from "react-native";
+import { View, StyleSheet } from "react-native";
 import Header from "../../components/Header";
 
 const Home = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Header />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+});
 
 export default Home;

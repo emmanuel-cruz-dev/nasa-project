b9a2231488bc2baa352c656e190d333b9a722b74
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-01-27 11:03:03 -0300

    PostImage cards terminadas

--- src/components/PostImage/PostImage.tsx
+++ src/components/PostImage/PostImage.tsx
@@ -4,16 +4,36 @@ import { PostImage as PostImageTypes } from "../../types";
 
 const PostImage: FC<PostImageTypes> = ({ title, date }) => {
   return (
-    <View>
-      <Text>{title}</Text>
-      <Text>{date}</Text>
-      <View>
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.date}>{date}</Text>
+      <View style={styles.buttonContainer}>
         <Button title="View" />
       </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "rgba(18,39,113,255)",
+    borderRadius: 20,
+    marginBottom: 12,
+    padding: 16,
+  },
+  title: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 12,
+  },
+  date: {
+    color: "#fff",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+  },
+});
 
 export default PostImage;

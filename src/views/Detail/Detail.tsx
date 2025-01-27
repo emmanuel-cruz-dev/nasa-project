f26eb44d31bee2f6faf97700ff2467de81037ec3
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-01-27 13:19:16 -0300

    estructura y estilos de Detail component

--- src/views/Detail/Detail.tsx
+++ src/views/Detail/Detail.tsx
@@ -1,12 +1,39 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
+import { useRoute } from "@react-navigation/native";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RouteStackParams } from "../../types";
+import Header from "../../components/Header";
 
 const Detail = () => {
+  const {
+    params: { title, date, url, explanation },
+  } = useRoute<NativeStackScreenProps<RouteStackParams, "Detail">["route"]>();
+
   return (
-    <View>
-      <Text>Detail</Text>
+    <View style={styles.container}>
+      <Header />
+      <View style={styles.content}>
+        <Image source={{ uri: url }} style={styles.image} />
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.date}>{date}</Text>
+        <Text style={styles.explanation}>{explanation}</Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+    backgroundColor: "rgba(7,26,93,255)",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+});
 
 export default Detail;
